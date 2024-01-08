459bd974ca4ba2199b3f4968e267820df1a8c509
Nikita <ezin.nik@yandex.ru>
2024-01-08 17:21:03 +0700

    12.Опциональные параметры в функциях

---
+++ Lessons/12.ts
@@ -0,0 +1,9 @@
+function filter(numbers: number[], callback: (n: number) => boolean): number[] {
+  const result: number[] = [];
+  numbers.forEach((n) => {
+    if (callback(n)) {
+      result.push(n);
+    }
+  });
+  return result;
+}
