5e4a2ace7f82b20d050e40eff558dac4ccbc23dd
Nikita <ezin.nik@yandex.ru>
2024-01-09 11:11:17 +0700

    19.Перегрузка функций

---
+++ Lessons/19.ts
@@ -0,0 +1,8 @@
+function newYearCongratulate(name: string): string;
+function newYearCongratulate(year: number, name: string): string;
+function newYearCongratulate(data1: string | number, data2?: string): string {
+  if (typeof data1 === 'number') {
+    return `Hi ${data2}! Happy New Year ${data1}!`;
+  }
+  return `Hi ${data1}! Happy New Year!`;
+}
