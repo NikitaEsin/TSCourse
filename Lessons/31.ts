71f8a1c2946ee65c302ea0b7643a8e081fc0d84f
Nikita <ezin.nik@yandex.ru>
2024-01-10 16:29:51 +0700

    31.Иерархия типов

---
+++ Lessons/31.ts
@@ -0,0 +1,28 @@
+type User = {
+  id: number,
+  name: string,
+  age: number,
+};
+
+type Friends = [number, number];
+
+export type UserResponse = {
+  users: User[],
+  friends: Friends[]
+};
+
+const defaultUser = { id: 0, name: '', age: 0 };
+
+const getUserFriends = (userResponseJSON: string, userId: number): User[] => {
+  const userResponse = JSON.parse(userResponseJSON) as UserResponse;
+
+  return userResponse.friends
+    .map(([ownerId, friendId]: Friends): User => {
+      if (!(userId === ownerId || userId === friendId)) return defaultUser;
+      const searchId = (ownerId === userId) ? friendId : ownerId;
+      const friend: User | undefined = userResponse.users.find(({ id }) => id === searchId);
+
+      return friend === undefined ? defaultUser : friend;
+    })
+    .filter((user: User) => user.id > 0);
+};
